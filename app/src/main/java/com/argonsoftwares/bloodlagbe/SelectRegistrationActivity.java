5e66981bd8acd3c634a0ed6ca95af013bbd7386b
Samiul Islam <samiulislamsharan@gmail.com>
2022-01-13 05:00:20 +0600

    Blood Lagbe Initial commit.
    Includes 4 Activities with XML Layouts:
        1. LoginActivity
        2. SelectRegistrationActivity
        3. DonorRegistrationActivity
        4. RecipientRegistrationActivity

---
+++ app/src/main/java/com/argonsoftwares/bloodlagbe/SelectRegistrationActivity.java
@@ -0,0 +1,14 @@
+package com.argonsoftwares.bloodlagbe;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SelectRegistrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_registration);
+    }
+}
